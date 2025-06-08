036fbfbbd662b95a72e3cc8cde6a25c9da7da9cc
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-08 16:22:49 +0500

    Code Update!!

---
+++ src/Layout.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/common/Footer"
+import Header from "./components/common/Header"
+import { Outlet } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <>
+       <Header />
+       <Outlet />
+       <Footer />
+
+    </>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,53 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import './index.css'
+import Layout from './Layout.jsx'
+import Home from './components/screens/Home.jsx'
+import About from './components/screens/About.jsx'
+import Contact from './components/screens/Contact.jsx'
+import Github from './components/screens/Github.jsx'
+import User from './components/screens/User.jsx'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      
+      {
+        path: "about",
+        element: <About />
+      },
+      
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      
+      {
+        path: "user",
+        element: <User />
+      },
+      
+      {
+        path: "github",
+        element: <Github />
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={routes} />
+  </StrictMode>
 )
