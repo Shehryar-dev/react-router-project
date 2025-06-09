80334961e06e9788ce82138545e15365d2848434
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-09 18:48:06 +0500

    Code Update!

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 // import './index.css'
 import Layout from './Layout.jsx'
 import Home from './components/screens/Home.jsx'
@@ -8,41 +8,53 @@ import About from './components/screens/About.jsx'
 import Contact from './components/screens/Contact.jsx'
 import './index.css'
 
+// const routes = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+
+//       // {
+//       //   path: "user",
+//       //   element: <User />
+//       // },
+
+//       // {
+//       //   path: "github",
+//       //   element: <Github />
+//       // },
+//     ]
+//   }
+// ])
+
+
 const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      
-      {
-        path: "about",
-        element: <About />
-      },
-      
-      {
-        path: "contact",
-        element: <Contact />
-      },
-      
-      // {
-      //   path: "user",
-      //   element: <User />
-      // },
-      
-      // {
-      //   path: "github",
-      //   element: <Github />
-      // },
-    ]
-  }
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/user/:id' element={<Contact />} />
+    </Route>
+  )
 ])
 
 createRoot(document.getElementById('root')).render(
