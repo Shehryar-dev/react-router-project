e024d55aa5c7dd27670ef2ce5c6bcc8d086d75e2
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-10 22:01:17 +0500

    Code Updated

--- src/components/screens/Github.jsx
+++ src/components/screens/Github.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://api.github.com/users/Shehryar-dev')
+        .then(response => response.json()).then(data => {
+            setData(data);
+        })
+    })
     return (
         <div className='text-center'>
-            GitHub Data Fetch Soon
+          
+            GitHub Profile User Name: {data.login} <br/>
+            GitHub Profile Full Name: {data.name} <br/>
+            GitHub Followers: {data.followers} <br/>
+            GitHub bio: {data.bio} <br/>
+            GitHub Public Repo: {data.public_repos} <br/>
+            GitHub Followers: {data.following} <br/>
+            <img src={data.avatar_url} alt="" />
+            {/* {    "html_url": "https://github.com/Shehryar-dev",
+    "followers_url": "https://api.github.com/users/Shehryar-dev/followers",
+    "following_url": "https://api.github.com/users/Shehryar-dev/following{/other_user}",} */}
         </div>
     )
 }
 
 export default Github
+
 
